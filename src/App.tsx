326049540777d64890786a270fcc31bc4a3c39bf
deekshithmd <deekshithmogra@gmail.com>
2024-02-22 22:17:46 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Typography from "./components/Typography";
+
+export default function App() {
+  return (
+    <div>
+      <Typography variant="h2">Hello React</Typography>
+    </div>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.querySelector("#root");
+if (!rootElement) throw new Error("Cannot find root element with that id");
+
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
